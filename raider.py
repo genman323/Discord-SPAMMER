0d4f15ffc831eb9e49a8fd47f53b758d15bf401c
genman323 <genman323@gmail.com>
2025-05-15 17:10:34 -0700

    Update raider.py

--- raider.py
+++ raider.py
@@ -17,10 +17,10 @@ def load_token():
             data = json.load(file)
             return data.get("TOKEN")
     except FileNotFoundError:
-        print(Fore.RED + "Error: token.json not found.")
+        print(Fore.MAGENTA + "Error: token.json not found.")
         return None
     except json.JSONDecodeError:
-        print(Fore.RED + "Error: Invalid JSON format in token.json.")
+        print(Fore.MAGENTA + "Error: Invalid JSON format in token.json.")
         return None
 
 def display_logo():
@@ -28,40 +28,40 @@ def display_logo():
 -- VALIANCE RAIDS --
 '''
     os.system('cls' if os.name == 'nt' else 'clear')  
-    print(Fore.RED + logo)
+    print(Fore.MAGENTA + logo)
 
 def display_status(connected):
     if connected:
-        print(Fore.RED + "Status: Connected")
+        print(Fore.MAGENTA + "Status: Connected")
     else:
-        print(Fore.RED + "Status: Disconnected")
+        print(Fore.MAGENTA + "Status: Disconnected")
 
 def token_management():
     os.system('cls' if os.name == 'nt' else 'clear')  # Clear the console before showing token options
-    print(Fore.RED + "Welcome Valiance.\n")
+    print(Fore.MAGENTA + "Welcome Valiance.\n")
     print("The Valiance Raid Bot Is Not Currently Active.")
     print("Load or Enter You're New Desired Token To Access You're Bot.")
     
     # Adding an empty line between options and the input prompt
     print()
 
-    choice = input(Fore.RED + "New Token (1), Load Token(2): ")
+    choice = input(Fore.MAGENTA + "New Token (1), Load Token(2): ")
 
     if choice == "1":
-        new_token = input(Fore.RED + "Load new token: ")
+        new_token = input(Fore.MAGENTA + "Load new token: ")
         save_token(new_token)
-        print(Fore.RED + "Token Saved!")
+        print(Fore.MAGENTA + "Token Saved!")
         return new_token
     elif choice == "2":
         token = load_token()
         if token:
-            print(Fore.RED + f"Token loaded: {token}")
+            print(Fore.MAGENTA + f"Token loaded: {token}")
             return token
         else:
-            print(Fore.RED + "Token Not Found")
+            print(Fore.MAGENTA + "Token Not Found")
             return None
     else:
-        print(Fore.RED + "Invalid choice. Please try again.")
+        print(Fore.MAGENTA + "Invalid choice. Please try again.")
         return None
 
 
@@ -98,7 +98,7 @@ async def spamraid(interaction: discord.Interaction):
 
 @bot.tree.command(name="gif_raid", description="Gif Raid")
 async def spamraid(interaction: discord.Interaction):
-    message_text = "https://media.discordapp.net/attachments/1371076001198637136/1372027134863736893/raIDSbot177.gif?ex=68254757&is=6823f5d7&hm=8e6b520a0823dd1ef71843ef0ca681028e698b2fedd00b6b2fa4de7e1cf4a4ab&=&width=979&height=968 https://media.discordapp.net/attachments/1370900523578032140/1372042255032127508/hah.gif?ex=6825556c&is=682403ec&hm=ab4a58bb3f05e6d153b3d8ae256aaee76c5cc69dd21a7de308ae5c3347f8a913&="
+    message_text = "https://media.discordapp.net/attachments/1371076001198637136/1372027134863736893/raIDSbot177.gif?ex=68254757&is=6823f5d7&hm=8e6b520a0823dd1ef71843ef0ca681028e698b2fedd00b6b2fa4de7e1cf4a4ab&=&width=979&height=968 "
     view = SpamButton(message_text)
     await interaction.response.send_message(f"🚨 Do You Want To Proceed?", view=view, ephemeral=True)
 
@@ -116,7 +116,7 @@ async def spamraid(interaction: discord.Interaction):
 
 @bot.tree.command(name="laughing_raid", description="Laughing Raid.")
 async def spamraid(interaction: discord.Interaction):
-    message_text = "## 😂𝔸𝕃𝕀𝔸ℕℂ𝔼 ℝ𝔸𝕀𝔻𝕊 ℝ𝔸ℙ𝔼𝔻 𝕐𝕆𝕌'ℝ𝔼 𝕊𝔼ℝ𝕍𝔼ℝ😂ℝ𝔸𝕀𝔻𝔼𝔻😂𝕍𝔸𝕃𝕀𝔸ℕℂ𝔼 ℝ𝔸𝕀𝔻𝕊 ℝ𝔸ℙ𝔼𝔻 𝕐𝕆𝕌'ℝ𝔼 𝕊𝔼ℝ𝕍𝔼ℝ😂 "
+    message_text = "## 😂ℝ𝔸ℙ𝔼𝔻😂ℝ𝔸ℙ𝔼𝔻 𝔹𝕐 𝕍𝔸𝕃𝕀𝔸ℕℂ𝔼 ℝ𝔸𝕀𝔻😂ℝ𝔸𝕀𝔻𝔼𝔻 𝔹𝕐 𝕍𝔸𝕃𝕀𝔸ℕℂ𝔼 ℝ𝔸𝕀𝔻𝕊😂ℝ𝔸ℙ𝔼𝔻😂 "
     view = SpamButton(message_text)
     await interaction.response.send_message(f"🚨 Do You Want To Proceed?", view=view, ephemeral=True)
 
@@ -134,14 +134,14 @@ async def spamraid(interaction: discord.Interaction):
 async def on_ready():
     display_logo()
     display_status(True)
-    print("You are connected! " + Fore.RED + f"{bot.user}")
+    print("You are connected! " + Fore.MAGENTA + f"{bot.user}")
 
     try:
         await bot.tree.sync()  
-        print(Fore.RED + "Bot is active!")
+        print(Fore.MAGENTA + "Bot is active!")
     except Exception as e:
         display_status(False)
-        print(Fore.RED + f"Error during synchronization: {e}")
+        print(Fore.MAGENTA + f"Error during synchronization: {e}")
 
 if __name__ == "__main__":
     TOKEN = token_management()
@@ -149,16 +149,16 @@ if __name__ == "__main__":
         try:
             bot.run(TOKEN)
         except discord.errors.LoginFailure:
-            print(Fore.RED + "Can't connect to token. Please check your token.")
-            input(Fore.RED + "Press Enter to go back to the menu...")
+            print(Fore.MAGENTA + "Can't connect to token. Please check your token.")
+            input(Fore.MAGENTA + "Press Enter to go back to the menu...")
             TOKEN = token_management()  # Restart the token selection process
             if TOKEN:
                 bot.run(TOKEN)  # Run again with the new token
         except Exception as e:
-            print(Fore.RED + f"An unexpected error occurred: {e}")
-            input(Fore.RED + "Press Enter to restart the menu...")
+            print(Fore.MAGENTA + f"An unexpected error occurred: {e}")
+            input(Fore.MAGENTA + "If you press enter, you can restart the menu.")
             TOKEN = token_management()  # Restart the token selection process
             if TOKEN:
                 bot.run(TOKEN)  # Run again with the new token
     else:
-        print(Fore.RED + "❌ Error: Unable to load or set a token.")
+        print(Fore.MAGENTA + "❌ Error: Unable to load or set a token.")
